6cac5d3c7d51a9a27e64b5d207ba4e5037f11e01
Durant Thorvald <durantthorvals@gmail.com>
2021-12-03 03:55:26 -0800

    work on recover_test()

--- examples/buildAndRun.sh
+++ examples/buildAndRun.sh
@@ -2,11 +2,11 @@
 file=Goprogramming.pdf
 go build -o main ./main.go ./flag.go 
 # init the system
-./main -md init -k 12 -m 4 -dn 16 -bs 1048576
+./main -md init -k 12 -m 4 -dn 24 -bs 1048576
 
 # A loopback system
-./main -md encode -f test/$file -conStripes 100 -o
-./main -md read -f $file -fn 0 -conStripes 100 -sp output/$file
+# ./main -md encode -f test/$file -conStripes 100 -o
+# ./main -md read -f $file -fn 0 -conStripes 100 -sp output/$file
 srchash=(`sha256sum test/$file|tr ' ' ' '`)
 dsthash=(`sha256sum output/$file|tr ' ' ' '`)
 echo $srchash
--- examples/main.go
+++ examples/main.go
@@ -98,3 +98,84 @@ func main() {
 	//It functions as a testbed, so currently I won't use goroutines.
 	log.Printf("%s consumes %.3f s", mode, time.Since(start).Seconds())
 }
+
+var (
+	blockSize         int64
+	mode              string
+	k                 int
+	m                 int
+	diskNum           int
+	filePath          string
+	savePath          string
+	newFilePath       string
+	new_k             int
+	new_m             int
+	recoveredDiskPath string
+	failMode          string
+	failNum           int
+	override          bool
+	conWrites         bool
+	conReads          bool
+	conStripes        int
+	replicateFactor   int
+	quiet             bool
+)
+
+//the parameter lists, with fullname or abbreviation
+func flag_init() {
+
+	flag.StringVar(&mode, "md", "encode", "the mode of ec system, one of (encode, decode, update, scaling, recover)")
+	flag.StringVar(&mode, "mode", "encode", "the mode of ec system, one of (encode, decode, update, scaling, recover)")
+
+	flag.IntVar(&k, "k", 12, "the number of data shards(<256)")
+	flag.IntVar(&k, "dataNum", 12, "the number of data shards(<256)")
+
+	flag.IntVar(&m, "m", 4, "the number of parity shards(2-4)")
+	flag.IntVar(&m, "parityNum", 4, "the number of parity shards(2-4)")
+
+	flag.Int64Var(&blockSize, "bs", 4096, "the block size in bytes")
+	flag.Int64Var(&blockSize, "blockSize", 4096, "the block size in bytes")
+
+	flag.IntVar(&conStripes, "cs", 100, "how many stripes are allowed to encode/decode concurrently")
+	flag.IntVar(&conStripes, "conStripes", 100, "how many stripes are allowed to encode/decode concurrently")
+
+	flag.IntVar(&diskNum, "dn", 4, "the number of disks used in .hdr.disk.path")
+	flag.IntVar(&diskNum, "diskNum", 4, "the number of disks used in .hdr.disk.path")
+
+	flag.StringVar(&filePath, "f", "", "upload: the local file path, download&update: the remote file name")
+	flag.StringVar(&filePath, "filePath", "", "upload: the local file path, download&update: the remote file name")
+
+	flag.StringVar(&savePath, "sp", "file.save", "the local saving path(local path)")
+	flag.StringVar(&savePath, "savePath", "file.save", "the local saving path(local path)")
+
+	flag.IntVar(&new_k, "new_k", 32, "the new number of data shards(<256)")
+	flag.IntVar(&new_k, "newDataNum", 32, "the new number of data shards(<256)")
+
+	flag.IntVar(&new_m, "new_m", 8, "the new number of parity shards(2-4)")
+	flag.IntVar(&new_m, "newParityNum", 8, "the new number of parity shards(2-4)")
+
+	flag.StringVar(&recoveredDiskPath, "rDP", "/tmp/restore", "the data path for recovered disk, default to /tmp/data")
+	flag.StringVar(&recoveredDiskPath, "recoverDiskPath", "/tmp/restore", "the data path for recovered disk, default to /tmp/data")
+
+	flag.BoolVar(&override, "o", false, "whether or not to override former files or directories, default to false")
+	flag.BoolVar(&override, "override", false, "whether or not to override former files or directories, default to false")
+
+	flag.BoolVar(&conWrites, "cw", true, "whether or not to enable concurrent write, default is false")
+	flag.BoolVar(&conWrites, "conWrites", true, "whether or not to enable concurrent write, default is false")
+
+	flag.BoolVar(&conReads, "cr", true, "whether or not to enable concurrent read, default is false")
+	flag.BoolVar(&conReads, "conReads", true, "whether or not to enable concurrent read, default is false")
+
+	flag.StringVar(&failMode, "fmd", "diskFail", "simulate [diskFail] or [bitRot] mode")
+	flag.StringVar(&failMode, "failMode", "diskFail", "simulate [diskFail] or [bitRot] mode")
+
+	flag.IntVar(&failNum, "fn", 0, "simulate multiple disk failure, provides the fail number of disks")
+	flag.IntVar(&failNum, "failNum", 0, "simulate multiple disk failure, provides the fail number of disks")
+
+	flag.IntVar(&replicateFactor, "rf", 3, "the meta data is replicated `rf`- fold to provide enough reliability, default is 3-fold")
+	flag.IntVar(&replicateFactor, "replicateFactor", 3, "the meta data is replicated `rf`- fold to provide enough reliability, default is 3-fold")
+
+	flag.BoolVar(&quiet, "q", false, "if true mute outputs otherwise print them")
+	flag.BoolVar(&quiet, "quiet", false, "if true mute outputs otherwise print them")
+
+}
