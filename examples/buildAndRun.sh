16ebf4e792f8f8235ced1a46b8d9954570da3b9d
DurantThorvalds <durant1997@foxmail.com>
2022-10-18 09:27:40 +0800

    work on gca-p

--- examples/buildAndRun.sh
+++ examples/buildAndRun.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-filename=test-64Mx32
-inputdir=~/input/
-outputdir=~/output/
+filename=test-64Mx320
+inputdir=/mnt/disk15/
+outputdir=/mnt/disk16/
 newfilename=new-64Mx16
 #data shards
 k=6
 #parity shards
 m=2
 #used disk number
-dn=32
+dn=12
 #block size
-bs=1048576
+bs=67108864
 #memory limit
 mem=8
 #failed disk number
@@ -30,34 +30,37 @@ start=`date +%s%N`
 now=`date +%c` 
 echo "The program starts at $now."  
 #------------------------encode a file--------------------------
-# init the system
-# ./main -md init -k $k -m $m -dn $dn -bs $bs -mem $mem
+mode="none"
+if [ $mode == "encode" ]; then
+    # init the system
+    ./main -md init -k $k -m $m -dn $dn -bs $bs -mem $mem
 
-# to encode a file 
-# ./main -md encode -f $inputdir$filename -conStripes 100 -o
+    # to encode a file 
+    ./main -md encode -f $inputdir$filename -conStripes 100 -o
 
 
-# to update a file
-# ./main -md update -f $filename -nf $newfilename
-# to read a file
-# ./main -md read -f $filename -conStripes 100 -sp $outputdir$filename
-# to remove a file
-# ./main -md delete -f $filename
+    # to update a file
+    # ./main -md update -f $filename -nf $newfilename
+    # to read a file
+    ./main -md read -f $filename -conStripes 100 -sp $outputdir$filename
+    # to remove a file
+    # ./main -md delete -f $filename
 
-# srchash=(`sha256sum $inputdir$filename|tr ' ' ' '`)
-# dsthash=(`sha256sum $outputdir$filename|tr ' ' ' '`)
-# echo $srchash
-# echo $dsthash
-# if [ $srchash == $dsthash ];then 
-#     echo "hash check succeeds"
-# else
-#     echo "hash check fails"
-# fi
-
+    srchash=(`sha256sum $inputdir$filename|tr ' ' ' '`) #6cb118a8f8b3c19385874297e291dcbcdf3a9837ba1ca7b00ace2491adbff551
+    dsthash=(`sha256sum $outputdir$filename|tr ' ' ' '`)
+    echo $srchash
+    echo $dsthash
+    if [ $srchash == $dsthash ];then 
+        echo "hash check succeeds"
+    else
+        echo "hash check fails"
+    fi
+else
 #---------------------------repair the file----------------------
-# recover a file
-# methods: baseline, sga, gca
-./main -md baseline -fmd diskFail -fd $fd -f $inputdir$filename -conStripes 100 -o
-end=`date +%s%N`
-cost=`echo $start $end | awk '{ printf("%.3f", ($2-$1)/1000000000) }'`
-echo "sh: previous procedure consumed $cost s"
+    # recover a file
+    # methods: baseline, sga, gca
+    ./main -md gca -fmd diskFail -fd $fd -f $inputdir$filename -conStripes 100 -o
+    end=`date +%s%N`
+    cost=`echo $start $end | awk '{ printf("%.3f", ($2-$1)/1000000000) }'`
+    echo "sh: previous procedure consumed $cost s"
+fi
